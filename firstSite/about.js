7a93cdb73654fc8cdaa53d93a1bbb47a3297f200
Izah Sohail <izahs2003@gmail.com>
2023-09-24 23:41:30 +0400

    update

--- firstSite/about.js
+++
@@ -1,9 +0,0 @@
-// Toggle dark mode
-function toggleDarkMode() {
-    const body = document.body;
-    body.classList.toggle("dark-mode");
-}
-
-// Add event listener for a button click (you can add a button for toggling)
-const darkModeButton = document.getElementById("dark-mode-button");
-darkModeButton.addEventListener("click", toggleDarkMode);
