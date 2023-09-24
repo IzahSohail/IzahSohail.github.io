a6e9784b4418444de4193f77ab6b86b6bede40a3
Izah Sohail <izahs2003@gmail.com>
2023-09-24 04:08:31 +0400

    update

--- firstSite/script.js
+++ firstSite/script.js
@@ -4,15 +4,20 @@ const mainContent = document.querySelector('.main-content');
 // Calculate the offset from the top of the page where you want the div to start moving
 const scrollOffset = 20; // Adjust this value as needed
 
+// Calculate the maximum scroll limit
+const maxScroll = 100; // Adjust this value as needed
+
 // Listen to the scroll event
 window.addEventListener('scroll', () => {
   // Get the current scroll position
   const scrollY = window.scrollY;
 
   // Calculate the new position for the "main-content" element
-  const newPosition = Math.max(0, scrollY - scrollOffset);
+  let newPosition = Math.max(0, scrollY - scrollOffset);
+
+  // Limit the scrolling to a maximum scroll limit
+  newPosition = Math.min(newPosition, maxScroll);
 
   // Apply the new position as a style to move the element up
   mainContent.style.transform = `translateY(-${newPosition}px)`;
 });
-
