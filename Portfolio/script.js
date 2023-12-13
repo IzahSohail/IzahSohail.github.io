de091923813c693668148d5446555e71e0e6c260
Izah Sohail <izahs2003@gmail.com>
2023-12-13 09:33:46 +0400

    u

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -78,21 +78,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Show the initial project
     showProject();
-
-    var swiper = new Swiper(".swiper", {
-        spaceBetween: 30,
-        centeredSlides: true,
-        loop: true,
-        autoplay: {
-          delay: 4000,
-          disableOnInteraction: false,
-        },
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-      });
-      
 });
 
 document.addEventListener("DOMContentLoaded", function () {
